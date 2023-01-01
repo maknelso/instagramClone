758d0618b6c6e43f4cab644642a155fd9525dbfb
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-01 14:58:12 -0800

    successfully uploaded image to S3

--- Server/handler/generate_upload_URL.py
+++ Server/handler/generate_upload_URL.py
@@ -2,38 +2,34 @@ from flask_restful import Resource, marshal_with, fields
 import logging
 import boto3
 from botocore.exceptions import ClientError
-from flask import Flask, request
+from flask import Flask, request, abort
+import json
 
 uploadFields = {
-    "url": fields.String,
+    "res": fields.String,
 }
 
 
 class generate_upload_URL(Resource):
     @marshal_with(uploadFields)
     def get(self):
-        print(request.args)
-        object_name = request.args.get("filename")
-        """Generate a presigned URL to share an S3 object
+        # get filename
+        filename = request.args.get("filename")
 
-      :param bucket_name: string
-      :param object_name: string
-      :param expiration: Time in seconds for the presigned URL to remain valid
-      :return: Presigned URL as string. If error, returns None.
-      """
-
-        # Generate a presigned URL for the S3 object
+        # init s3 client
         s3_client = boto3.client("s3")
         try:
-            response = s3_client.generate_presigned_url(
-                "put_object",
-                Params={"Bucket": "instagramclone-2022", "Key": object_name},
-                ExpiresIn=3600,
-                HttpMethod="put"
+            # get presigned url
+            response = s3_client.generate_presigned_post(
+                "instagramclone-2022", "upload/" + filename, ExpiresIn=3600
             )
+            print(response)
+        # handle error
         except ClientError as e:
             logging.error(e)
-            return None
 
-        # The response contains the presigned URL
-        return {"url": response}
+            # return failed response
+            return abort(400)
+
+        # return successful response
+        return {"res": json.dumps(response)}
