e0761098e1e43dbf1579218ee43b2c6da079e13d
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-24 15:59:41 -0800

    changed endpoint url to production, ready to deploy FE to EC2

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-export const postAPI = (url, body = {}, headers = {}) => {
+const REACT_APP_BASEURL_PRO = process.env.REACT_APP_BASEURL_PRO;
+
+export const postAPI = (url, body = {}) => {
   const token = sessionStorage.getItem('token');
   const baseHeader = {
     Authorization: 'Bearer ' + token,
   };
-  return axios.post(url, body, { headers: baseHeader });
+  console.log(REACT_APP_BASEURL_PRO + url);
+
+  return axios.post(REACT_APP_BASEURL_PRO + url, body, { headers: baseHeader });
 };
 
 export const getAPI = (url) => {
@@ -13,7 +17,7 @@ export const getAPI = (url) => {
   const baseHeader = {
     Authorization: 'Bearer ' + token,
   };
-  return axios.get(url, {
+  return axios.get(REACT_APP_BASEURL_PRO + url, {
     headers: baseHeader,
   });
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./components/ThemeColor";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './components/ThemeColor';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <App />
